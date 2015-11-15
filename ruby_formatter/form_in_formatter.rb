bbe9171f89c6bd780ddaeb9cd5c2390ec79ce63c
matt.frizzell <mfrizzell@adblocks.com>
2015-11-15 01:29:24 -0500

    Completes lesson

--- ruby_formatter/form_in_formatter.rb
+++ ruby_formatter/form_in_formatter.rb
@@ -1,13 +1,13 @@
 print "What is your first name?"
-first_name = gets.chomp
+first_name = gets.chomp.capitalize!
 
 print "What is your last name?"
-last_name = gets.chomp
+last_name = gets.chomp.capitalize!
 
 print "What city are you from?"
-city = gets.chomp
+city = gets.chomp.capitalize!
 
 print "What state are you from? (e.g. NY)"
-state = gets.chomp
+state = gets.chomp.upcase!
 
 puts "Your name is #{first_name} #{last_name} and you are from #{city}, #{state}"
