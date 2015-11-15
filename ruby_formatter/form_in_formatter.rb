b85f02acc2786d9d8cadf4cd37f38ecef7f2069f
matt.frizzell <mfrizzell@adblocks.com>
2015-11-15 01:19:18 -0500

    Up through Step 5 of "Putting the form in formatter" lesson

---
+++ ruby_formatter/form_in_formatter.rb
@@ -0,0 +1,13 @@
+print "What is your first name?"
+first_name = gets.chomp
+
+print "What is your last name?"
+last_name = gets.chomp
+
+print "What city are you from?"
+city = gets.chomp
+
+print "What state are you from? (e.g. NY)"
+state = gets.chomp
+
+puts "Your name is #{first_name} #{last_name} and you are from #{city}, #{state}"
