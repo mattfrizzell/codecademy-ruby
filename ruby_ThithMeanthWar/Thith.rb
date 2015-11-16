16c5472f054ff8ee0bd8507cad9ce57cd89ec2dc
matt.frizzell <mfrizzell@adblocks.com>
2015-11-16 15:51:56 -0500

    Cleaned up failure dialog

--- ruby_ThithMeanthWar/Thith.rb
+++ ruby_ThithMeanthWar/Thith.rb
@@ -7,5 +7,5 @@ if user_input.include? "s"
    user_input.gsub!(/s/, "th")
    puts "What doeth thith even mean: '#{user_input}'?"
 else
-    print "Well that stinks, you didn't tell me anything that included the letter 'S'. :("
+    print "Well that stinks, you didn't tell me anything that included the letter 'S'."
 end
