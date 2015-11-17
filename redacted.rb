8ce2c8fbededdaf3f97cfd107f7446ca63b811d3
matt.frizzell <mfrizzell@adblocks.com>
2015-11-16 21:36:30 -0500

    Completes extra challenges     -What could you do to make sure your redactor redacts a word regardless of whether it's upper case or lower case?     -How could you make your program take multiple, separate words to REDACT?     -How might you make a new redacted string and save it as a variable, rather than just printing it to the console?

--- redacted.rb
+++ redacted.rb
@@ -1,15 +1,27 @@
 puts "Give me phrase please."
 text = gets.chomp
 
-puts "Now what word should we strike from the record?"
+puts "Now what word(s) should we strike from the record? (Seperate mutliple words with a comma"
 redact = gets.chomp
+redacted_words = redact.split(",")
 
 words = text.split(" ")
 
+redacted_text = ""
+
 words.each {|word| 
-    if word == redact
-        print "REDACTED "
+    doit = false
+    redacted_words.each { |r_word|
+        if word.downcase == r_word.downcase
+            doit = true
+            break
+        end
+    }
+    
+    if doit
+        redacted_text += "REDACTED "
     else
-        print word + " "
+        redacted_text += word + " "
     end
 }
+print redacted_text
