36ef518eb84837cf8aa8b845810288a2813ab82e
matt.frizzell <mfrizzell@adblocks.com>
2015-11-17 22:55:32 -0500

    Completes Ruby Histogram program

--- create_a_histogram.rb
+++ create_a_histogram.rb
@@ -4,3 +4,16 @@ text = gets.chomp
 words = text.split(" ")
 
 frequencies = Hash.new(0)
+
+words.each do |word|
+   frequencies[word] += 1
+end
+
+frequencies = frequencies.sort_by do |word, count|
+    count
+end
+frequencies.reverse!
+
+frequencies.each {|word, word_count|
+    puts word + " " + word_count.to_s
+}
