dbd824da3ca2b080e2f62eb5fd10a8713f5dddb8
matt.frizzell <mattfrizzell@gmail.com>
2015-11-23 23:59:30 -0500

    More work on project

--- night_at_the_movies.rb
+++ night_at_the_movies.rb
@@ -7,9 +7,32 @@ choice = gets.chomp
 
 case choice
 when "add"
-    puts "Added!"
+    puts "Please specify a movie title."
+    title = gets.chomp
+    
+    puts "Now, on a scale of 1-100, what would you rate #{title}?"
+    rating = gets.chomp
+    
+    if !movies[title.to_sym]
+        movies[title.to_sym] = rating.to_i
+        puts "Successfully added the movie #{title}, with a rating of #{rating} to your collection!"
+    else
+        puts "Warhing: #{title} is already in your movie collection!"
+    end
+
 when "update"
-    puts "Updated!"
+    puts "Please specify a movie title."
+    title = gets.chomp
+    
+    if !movies[title.to_sym]
+        puts "Warhing: #{title} is not in your collection, so there is nothing to update."
+    else
+        puts "Now, on a scale of 1-100, what would you rate #{title}?"
+        rating = gets.chomp
+        movies[title.to_sym] = rating.to_i
+        puts "Successfully updated the movie #{title}, with a rating of #{rating}."
+    end
+    
 when "display"
     puts "Movies!"
 when "delete"
