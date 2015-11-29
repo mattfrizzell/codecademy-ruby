ece5d325ce3e5339c1d30690750077760dc68945
matt.frizzell <mattfrizzell@gmail.com>
2015-11-29 12:00:24 -0500

    Adds extras Fixes glaring pin check bug that their unit tester doesn't discover

--- bank_on_ruby.rb
+++ bank_on_ruby.rb
@@ -9,13 +9,26 @@ class Account
    
    public
    def display_balance(pin_number)
-       puts pin_number == @pin ? "Balance: $#{@balance}" : pin_error
+       puts pin_number == pin ? "Balance: $#{@balance}" : pin_error
    end
    
    def withdraw(pin_number, amount)
-      if pin_number == @pin
-          @balance -= amount
-          puts "Withdrew #{amount}. New balance: $#{balance}."
+      if pin_number == pin
+          if amount <= @balance
+            @balance -= amount
+            puts "Withdrew #{amount}. New balance: $#{balance}."
+          else
+              puts overdraw_error
+          end
+      else
+         puts pin_error 
+      end      
+   end
+   
+   def deposit(pin_number, amount)
+       if pin_number == pin
+          @balance += amount
+          puts "Deposited #{amount}. New balance: $#{balance}."
       else
          puts pin_error 
       end      
@@ -29,6 +42,11 @@ class Account
    def pin_error
       return "Access denied: incorrect PIN." 
    end
+   
+   def overdraw_error
+      return "Withdrawal denied: You requested more funds than currently have." 
+   end
 end
 
-checking_account = Account.new("mine", 5734)
+checking_account = Account.new("mine", 4200)
+checking_account.withdraw(1234, 5000)
