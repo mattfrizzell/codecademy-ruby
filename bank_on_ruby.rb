5ee4a5b5142e456a6a09c3b03d3808ef897d46ae
matt.frizzell <mattfrizzell@gmail.com>
2015-11-29 11:49:53 -0500

    Completes lessons with no extras

--- bank_on_ruby.rb
+++ bank_on_ruby.rb
@@ -6,4 +6,29 @@ class Account
        @name = name
        @balance = balance
    end
+   
+   public
+   def display_balance(pin_number)
+       puts pin_number == @pin ? "Balance: $#{@balance}" : pin_error
+   end
+   
+   def withdraw(pin_number, amount)
+      if pin_number == @pin
+          @balance -= amount
+          puts "Withdrew #{amount}. New balance: $#{balance}."
+      else
+         puts pin_error 
+      end      
+   end
+   
+   private
+   def pin
+    @pin = 1234
+   end
+   
+   def pin_error
+      return "Access denied: incorrect PIN." 
+   end
 end
+
+checking_account = Account.new("mine", 5734)
