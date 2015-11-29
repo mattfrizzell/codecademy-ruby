61ad685c23e19c3267d3f570b0dae5305f237bc1
matt.frizzell <mattfrizzell@gmail.com>
2015-11-28 22:43:58 -0500

    Completes the virtual computer (focus on classes)

--- class.rb
+++ class.rb
@@ -1,3 +1,23 @@
-class Person
+class Computer
+    @@users = {}
+    
+    def initialize(username, password)
+        @username = username
+        @password = password
+        @files = {}
         
+        @@users[username] = password
+    end
+    
+    def create(filename)
+        time = Time.now
+        @files[filename] = time
+        puts "#{username} just added the file #{filename} at #{time}"
+    end
+    
+    def Computer.get_users
+        return @@users
+    end
 end
+
+my_computer = Computer.new("me", "1234")
